89323745d5830c4d6a1a6695185173fa52b393d0
arcade15xyz <90238231+arcade15xyz@users.noreply.github.com>
2025-01-31 12:10:57 +0530

    Associative array

--- index.php
+++ index.php
@@ -1,4 +1,4 @@
-<?php
+<!-- <?php
 
   // $names = array('John','Jack','Jill');
   // $numbers = [1,2,3,4,5,6];
@@ -33,62 +33,129 @@
 
   // ARRAY FUNCTIONS
 
+  // $output = null;
+  // $ids = [10,22,15,45,67,33];
+  // $users = ['user1', 'user2', 'user3'];
+
+  // //count
+  // $output = count($ids);
+  
+  // //sort
+  // sort($ids);
+  // sort($users);
+
+  // //rsort
+  // rsort($ids);
+  // rsort($users);
+
+  // //array_push
+  // array_push($ids,100);
+  // array_push($users, 'user4');
+
+  // //array_pop
+  // array_pop($ids);
+  // array_pop($users);
+
+  // //array_shift
+  // array_shift($ids);
+  // array_shift($users);
+
+  // //array_unshift
+  // array_unshift($ids, 100);
+  // array_unshift($users, 'users4');
+
+  // //array_slice
+  // $ids2 = array_slice($ids, 2 ,3);
+  // // var_dump($ids2);
+
+  // //array_splice
+  // array_splice($ids,1,1,'new id');
+  // array_splice($users, 0, 1, 'New Users');
+
+  // //array_sum
+  // $output = 'sum of ids: ' . array_sum($ids);
+
+  // //array_search
+  // $output = "User 2 is at index: " .array_search('user2', $users);
+
+  // //in_array
+  // $output = 'User 3 Exists: '. in_array('user3', $users);
+  
+  // //explode
+  // $tags = 'tech,code,programming';
+  // $tagsArr = explode(',',$tags);
+  // var_dump($tagsArr);
+
+  // //implode
+  // $output = implode(',',$users);
+
+
+
+
+
+  ?> -->
+
+
+
+<!-- <!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <script src="https://cdn.tailwindcss.com"></script>
+    <title>PHP From Scratch</title>
+</head>
+
+<body class="bg-gray-100">
+    <header class="bg-blue-500 text-white p-4">
+        <div class="container mx-auto">
+            <h1 class="text-3xl font-semibold">PHP From Scratch</h1>
+        </div>
+    </header>
+    <div class="container mx-auto p-4 mt-4">
+        <div class="bg-white rounded-lg shadow-md p-6 mt-6">
+            <p class="text-xl"><?= $output ?></p>
+            <h2 class="text-xl font-semibold my-4">IDs Array:</h2>
+            <p>
+              <pre>
+                <?php print_r($ids); ?>
+              </pre>
+            </p>
+            <h2 class="text-xl font-semibold my-4">Users Array:</h2>
+            <p>
+              <pre>
+                <?php print_r($users); ?>
+              </pre>
+            </p>
+        </div>
+    </div>
+</body>
+
+</html> -->
+
+
+
+
+
+<?php
+
   $output = null;
-  $ids = [10,22,15,45,67,33];
-  $users = ['user1', 'user2', 'user3'];
 
-  //count
-  $output = count($ids);
-  
-  //sort
-  sort($ids);
-  sort($users);
+  $user = [
+    'name' => 'John',
+    'email' => 'john@gmail.com',
+    'password' => '123456',
+    'hobbies' =>['Tennis','Video Games']
+  ];
 
-  //rsort
-  rsort($ids);
-  rsort($users);
+  $output = $user['name'];
+  $output = $user['email'];
 
-  //array_push
-  array_push($ids,100);
-  array_push($users, 'user4');
+  $output = $user['hobbies'][0];
+  $user['address'] = '123 Main St';
 
-  //array_pop
-  array_pop($ids);
-  array_pop($users);
-
-  //array_shift
-  array_shift($ids);
-  array_shift($users);
-
-  //array_unshift
-  array_unshift($ids, 100);
-  array_unshift($users, 'users4');
-
-  //array_slice
-  $ids2 = array_slice($ids, 2 ,3);
-  // var_dump($ids2);
-
-  //array_splice
-  array_splice($ids,1,1,'new id');
-  array_splice($users, 0, 1, 'New Users');
-
-  //array_sum
-  $output = 'sum of ids: ' . array_sum($ids);
-
-  //array_search
-  $output = "User 2 is at index: " .array_search('user2', $users);
-
-  //in_array
-  $output = 'User 3 Exists: '. in_array('user3', $users);
-  
-  //explode
-  $tags = 'tech,code,programming';
-  $tagsArr = explode(',',$tags);
-  var_dump($tagsArr);
-
-  //implode
-  $output = implode(',',$users);
-  ?>
+?>
 
 
 
@@ -112,18 +179,10 @@
         <div class="bg-white rounded-lg shadow-md p-6 mt-6">
             <!-- Output -->
             <p class="text-xl"><?= $output ?></p>
-            <h2 class="text-xl font-semibold my-4">IDs Array:</h2>
-            <p>
-              <pre>
-                <?php print_r($ids); ?>
-              </pre>
-            </p>
-            <h2 class="text-xl font-semibold my-4">Users Array:</h2>
-            <p>
-              <pre>
-                <?php print_r($users); ?>
-              </pre>
-            </p>
+            <h2 class="text-xl font-semibold my-4">User Array:</h2>
+            <pre>
+              <?php print_r($user) ?>
+            </pre>
         </div>
     </div>
 </body>
